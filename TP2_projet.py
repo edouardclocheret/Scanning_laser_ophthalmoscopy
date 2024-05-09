de0cb5d999f4d27179e5d42cb9ac04b7b2ed7091
clocheret <edouard.clocheret@ensta-paris.fr>
2024-05-09 12:14:30 +0200

    Premiers essais de segmentation

--- TP2_projet.py
+++ TP2_projet.py
@@ -10,13 +10,13 @@ from skimage import data, filters
 from matplotlib import pyplot as plt
 
 
-def seuillage(img,img_mask, seuil):
+def seuillage(img,img_mask,seuil):
     
     img_out = (img_mask & (img < seuil))
     return img_out
 
 
-def seuillages_successifs (img):
+def seuillages_successifs (img, img_mask):
     #Cette fonction a pour but d'étudier quelle valeur de seuil est optimale
     
     for i in range(8):
@@ -31,18 +31,91 @@ def seuillages_successifs (img):
     plt.show()
 
 
+def top_hat (img, elem_struct) :
+    dilate = dilation(img)
+    return img ^ dilate
 
-def opening(img) :
-    return 
+
+def my_segmentation_test_boucles_pas_ouf(img, img_mask):
+    print("seg")
+    #Image Verite Terrain en booleen
+    img_GT =  np.asarray(Image.open('./images_IOSTAR/GT_01.png')).astype(np.bool_)
+    
+    img_out = img
+    
+    
+    seuil = 70
+    img_out = seuillage(img,img_mask,seuil)
+    ACCU, RECALL, img_out_skel, GT_skel = evaluate(img_out, img_GT)
+
+    
+    
+    print("Top_hat")
+    for i in range(1,10,1):
+        for j in range(1,10,1):
+            elem = rectangle(i,j)
+            
+
+            img_out_test= white_tophat(img,elem)
+            disk_ = disk(3)
+            img_out = closing(img_out, disk_)
 
 
-def my_segmentation(img, img_mask, seuil):
 
-    img_out = seuillage(img,img_mask, seuil)
+            ACCU_test, RECALL_test, img_out_skel, GT_skel = evaluate(img_out_test, img_GT)
+            #reconnecter les vaisseaux
+    
+            if RECALL_test > RECALL:
+                img_out = img_out +img_out_test
+                ACCU = ACCU_test
+                RECALL = RECALL_test
+                print("amelioration")
 
-    return img_out
+            print(i,j)
+    
+    
+            
+    
     
 
+            
+            
+    
+    
+    return img_out
+    
+def my_segmentation(img, img_mask):
+
+    img_out = img
+    
+    
+    
+    #objectif : mettre en valeur les structures linéaires
+    elem = rectangle(1,50)
+    elem_2 = rectangle(50,1)
+    elem_3 = np.eye(50, dtype=bool)
+    img_out= black_tophat(img_out,elem)+black_tophat(img_out,elem_2)    
+    
+    #disk_elem = disk(2)
+    #img_out = closing(img_out, disk_elem)
+  
+    #objectif : supprimer les points isolés
+    img_out = opening(img_out, disk(2))
+
+    seuil = 20
+    img_out = seuillage(img_out,img_mask,seuil)
+
+    
+    
+    
+    #applique le masque
+    seuil = 1
+    img_out = seuillage(img_out,img_mask,seuil)
+    
+    plt.imshow(img_out)
+    plt.show()
+    
+    return img_out
 
 
 
@@ -58,29 +131,8 @@ def evaluate(img_out, img_GT):
     RECALL = TP / (TP + FN) # Rappel
     return ACCU, RECALL, img_out_skel, GT_skel
 
-#Ouvrir l'image originale en niveau de gris
-img =  np.asarray(Image.open('./images_IOSTAR/star01_OSC.jpg')).astype(np.uint8)
-print(img.shape)
 
-nrows, ncols = img.shape
-row, col = np.ogrid[:nrows, :ncols]
-#On ne considere que les pixels dans le disque inscrit 
-img_mask = (np.ones(img.shape)).astype(np.bool_)
-invalid_pixels = ((row - nrows/2)**2 + (col - ncols/2)**2 > (nrows / 2)**2)
-img_mask[invalid_pixels] = 0
-
-img_out = my_segmentation(img,img_mask,80)
-
-#Ouvrir l'image Verite Terrain en booleen
-img_GT =  np.asarray(Image.open('./images_IOSTAR/GT_01.png')).astype(np.bool_)
-print(img_GT.shape)
-ACCU, RECALL, img_out_skel, GT_skel = evaluate(img_out, img_GT)
-print('Accuracy =', ACCU,', Recall =', RECALL)
-
-
-
-
-def affichage(img,img_out,img_out_skel,img_GT,GT_skel):
+def affichage_my_seg(img,img_out,img_out_skel,img_GT,GT_skel):
     plt.subplot(231)
     plt.imshow(img,cmap = 'gray')
     plt.title('Image Originale')
@@ -99,5 +151,29 @@ def affichage(img,img_out,img_out_skel,img_GT,GT_skel):
     plt.show()
 
 
-seuillages_successifs(img)
-affichage(img,img_out,img_out_skel,img_GT,GT_skel)
+def main() : 
+    #Ouvrir l'image originale en niveau de gris
+    img =  np.asarray(Image.open('./images_IOSTAR/star01_OSC.jpg')).astype(np.uint8)
+    nrows, ncols = img.shape
+    row, col = np.ogrid[:nrows, :ncols]
+    
+    #On ne considere que les pixels dans le disque inscrit 
+    img_mask = (np.ones(img.shape)).astype(np.bool_)
+    invalid_pixels = ((row - nrows/2)**2 + (col - ncols/2)**2 > (nrows / 2)**2)
+    img_mask[invalid_pixels] = 0
+
+    img_out = my_segmentation(img,img_mask)
+
+    #Ouvrir l'image Verite Terrain en booleen
+    img_GT =  np.asarray(Image.open('./images_IOSTAR/GT_01.png')).astype(np.bool_)
+    
+    ACCU, RECALL, img_out_skel, GT_skel = evaluate(img_out, img_GT)
+    
+
+    #affichage_my_seg(img,img_out,img_out_skel,img_GT,GT_skel)
+    print("Accuracy = ", ACCU, "Recall =", RECALL)
+    
+
+
+if __name__ == "__main__" :
+    main()
